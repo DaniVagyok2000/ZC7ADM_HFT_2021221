01bb2f92d6faa5cdd5771923d324ecfb9c5ef2ba
Fogarasi Daniel <fogardani@gmail.com>
2021-10-17 21:45:55 +0200

    Test

--- ZC7ADM_HFT_2021221.Client/Program.cs
+++ ZC7ADM_HFT_2021221.Client/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
+
 namespace ZC7ADM_HFT_2021221.Client
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
---
+++ ZC7ADM_HFT_2021221.Test/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZC7ADM_HFT_2021221.Data;
+
+namespace ZC7ADM_HFT_2021221.Test
+{
+    public class Test
+    {
+       public RestaurantDbContext ctx = new RestaurantDbContext();
+
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
