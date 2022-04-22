dda8c5c07fb38487bffdeb2a29fea0e6d2c45775
danivagyok2000@stud.uni-obuda.hu <danivagyok2000@stud.uni-obuda.hu>
2022-04-22 16:11:29 +0200

    Update?

--- ZC7ADM_HFT_2021221.JSClient/wwwroot/script.js
+++ ZC7ADM_HFT_2021221.JSClient/wwwroot/script.js
@@ -18,8 +18,8 @@ function display() {
     employees.forEach(t =>
     {
         document.getElementById('resultarea')
-            .innerHTML += "<tr><td>" + t.name + "</td><td>" + t.salary + "</td><td>" + t.employeeId + "</td><td>" +
-            `<button type="button" onclick="remove(${t.employeeId})">Delete</button>`
+            .innerHTML += "<tr><td>" + t.name + "</td><td>" + t.salary + "</td><td>" + t.restaurantId + "</td><td>" +
+            `<button type="button" onclick="remove(${t.employeeId})">Delete</button>` + ","+ `<button type="button" onclick="update(${ t.employeeId })">Update</button>`
              +"</td></tr>";
     })
 }
@@ -37,6 +37,34 @@ function remove(id) {
         })
         .catch((error) => { console.error('Error:', error); });
 
+}
+
+function update(id)
+{
+    let Name = document.getElementById('employeename').value;
+    let sal = document.getElementById('employeesalary').value;
+    let restId = document.getElementById('restaurantid').value;
+    fetch('http://localhost:31877/employee/' + id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                name: Name,
+                salary: sal,
+                restaurantId: restId,
+                employeeId: id
+            }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            getData();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
 
 function create()
